3d356df06744e5a1728ce35ca92e0ba9a01e6da3
mjay <michael@merged.group>
2024-03-04 21:57:31 -0500

    updated styling

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const showFooter = computed(() => !route.meta.hideFooter);
 <template>
   <div id="app-container">
     <div id="content">
-      <div id="router-view-container" class="p-4 sm:p-8 md:p-0">
+      <div id="router-view-container" class="flex-1 p-4 sm:p-8 md:p-0">
         <RouterView />
       </div>
     </div>
