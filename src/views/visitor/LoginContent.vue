eeba2bdc016570306e3abefbe0f9924b8cf68026
mjay <michael@merged.group>
2024-03-06 12:48:03 -0500

    Pulled out common LoginContent into separate component

--- src/views/visitor/LoginContent.vue
+++ src/views/visitor/LoginContent.vue
@@ -11,7 +11,6 @@ import type { User } from 'firebase/auth';
 import { useUserStore } from '@/stores/user';
 import { apiLogin } from '@/services/apiService';
 import ComingSoon from '@/components/visitor/ComingSoon.vue';
-import LoginContent from './LoginContent.vue';
 
 defineExpose({ EyeIcon, EyeSlashIcon, ErrorAlert, FinishOnboardingPrompt });
 
@@ -94,24 +93,65 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    id="login-container"
-    class="flex-1 min-h-full px-4 py-1 sm:py-0"
-    :class="{
-      'flex flex-col items-center justify-center': !showSplitContent,
-      'flex flex-row items-start justify-center space-x-4': showSplitContent
-    }"
-  >
-    <!-- NEW CONTENT -->
-    <div v-if="showSplitContent" class="w-1/2">
-      <ComingSoon />
+  <div id="existing-login">
+    <div class="absolute top-0 right-0 pt-3 pr-3">
+      <button @click="handleHomeClick" class="text-caramel-500 hover:text-caramel-600">
+        <HomeIcon class="h-8 w-8 text-accents-500" />
+      </button>
     </div>
-
-    <!-- PREVIOUSLY EXISTING CONTENT -->
-    <LoginContent />
-
-    <FinishOnboardingPrompt :show="showFinishOnboardingPrompt" :closeModal="closeModal" />
-    <ErrorAlert :message="errorMessage" v-model:isVisible="showError" />
+    <LogoContainer class="py-5" imgClass="h-28" />
+    <h2 class="text-2xl text-accents-500 font-light my-2">Welcome Back!</h2>
+    <div class="p-5 sm:p-6 max-w-md w-full bg-white rounded-xl shadow-md">
+      <form @submit.prevent="handleLogin" class="login-form space-y-5">
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700">Email:</label>
+          <input id="email" v-model="form.email" type="email" required />
+        </div>
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
+          <div class="relative mt-1">
+            <input
+              id="password"
+              v-model="form.password"
+              :type="passwordVisible ? 'text' : 'password'"
+              required
+            />
+            <button
+              type="button"
+              @click="togglePasswordVisibility"
+              class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500"
+              tabindex="-1"
+            >
+              <EyeIcon v-if="!passwordVisible" class="h-5 w-5" />
+              <EyeSlashIcon v-else class="h-5 w-5" />
+            </button>
+          </div>
+        </div>
+        <div>
+          <button
+            id="login-button"
+            type="submit"
+            :disabled="!isInputValid"
+            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-caramel-600 hover:bg-caramel-700 disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            Log In
+          </button>
+        </div>
+        <div class="flex justify-center">
+          <button @click="handleSignUp" class="text-sage-500 hover:text-sage-600 cursor-pointer">
+            Don't have an account? Sign Up
+          </button>
+        </div>
+        <div class="text-center my-2">
+          <a
+            @click="handleForgotPassword"
+            class="text-sm text-sage-500 hover:text-sage-600 cursor-pointer"
+          >
+            Forgot your password?
+          </a>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -166,4 +206,3 @@ onMounted(async () => {
   }
 }
 </style>
-@/stores/screen
